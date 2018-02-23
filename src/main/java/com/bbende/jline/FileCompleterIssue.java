151576499f2b6390d1c3acad30817fce5e45eb4a
André Wolski <locke@dena-design.de>
2018-02-23 18:12:55 +0100

    cleanup, reduce to FileNameCompleter

--- src/main/java/com/bbende/jline/FileCompleterIssue.java
+++ src/main/java/com/bbende/jline/FileCompleterIssue.java
@@ -28,36 +28,14 @@ public class FileCompleterIssue {
                 .nativeSignals(true)
                 .signalHandler(Terminal.SignalHandler.SIG_IGN)
                 .build();
-             PrintStream output = new PrintStream(terminal.output(), true)) {
+            PrintStream output = new PrintStream(terminal.output(), true)) {
 
-            // list of top-level nodes for tree completer
-            List<Completers.TreeCompleter.Node> rootNodes = new ArrayList<>();
-
-            // create a node for each command (cmd1, cmd2, cmd3
-            for (int i=1; i <= 3; i++) {
-                List<Object> nodeObjects = new ArrayList<>();
-                nodeObjects.add("cmd" + i);
-
-                // create a file name completer and a node for the completer
-                Completer fileNameCompleter = new Completers.FileNameCompleter();
-                Completers.TreeCompleter.Node fileNameNode = new Completers.TreeCompleter.Node(fileNameCompleter, Collections.emptyList());
-
-                // create a node for the -f argument with the file name completer node as a child
-                Completers.TreeCompleter.Node optionNode = Completers.TreeCompleter.node("-f", fileNameNode);
-                nodeObjects.add(optionNode);
-
-                // add the overall command node to the root nodes
-                Object[] nodeObjectsArray = nodeObjects.toArray(new Object[nodeObjects.size()]);
-                rootNodes.add(Completers.TreeCompleter.node(nodeObjectsArray));
-            }
-
-            // create the top-level completer
-            Completer completer = new Completers.TreeCompleter(rootNodes);
+            Completer fileNameCompleter = new Completers.FileNameCompleter();
 
             LineReader reader = LineReaderBuilder.builder()
                     .appName(SHELL_NAME)
                     .terminal(terminal)
-                    .completer(completer)
+                    .completer(fileNameCompleter)
                     .build();
 
             reader.setOpt(LineReader.Option.AUTO_FRESH_LINE);
